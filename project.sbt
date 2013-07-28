b83e88c2cd476ed629bdfed909aae5aa8919b77a
Johannes Rudolph <johannes.rudolph@gmail.com>
2013-07-28 13:01:26 +0200

    add publishing configuration

--- project.sbt
+++ project.sbt
@@ -1,5 +1,9 @@
-organization := "net.virtualvoid"
+organization := "net.virtual-void"
 
 name := "speed"
 
 version := "12"
+
+homepage := Some(url("http://github.com/jrudolph/speed"))
+
+licenses in GlobalScope += "Apache License 2.0" -> url("https://github.com/jrudolph/speed/raw/master/LICENSE")
---
+++ project/Helpers.scala
@@ -0,0 +1,14 @@
+object Helpers {
+  def generatePomExtra(scmUrl: String, scmConnection: String,
+                       developerId: String, developerName: String): xml.NodeSeq =
+    <scm>
+      <url>{ scmUrl }</url>
+      <connection>{ scmConnection }</connection>
+    </scm>
+    <developers>
+      <developer>
+        <id>{ developerId }</id>
+        <name>{ developerName }</name>
+      </developer>
+    </developers>
+}
