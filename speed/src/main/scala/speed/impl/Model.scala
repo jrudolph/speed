9fe8c6e00cd4788a68a5b1da4f7bc52a88cb830b
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-07-01 16:27:49 +0200

    move model into its own file

---
+++ speed/src/main/scala/speed/impl/Model.scala
@@ -0,0 +1,42 @@
+package speed.impl
+
+trait Model { self: SpeedImpl ⇒
+  import c.universe._
+
+  case class Closure(valName: TermName, application: Tree, init: Tree)
+  case class Closure2(valName1: TermName, valName2: TermName, application: Tree, init: Tree)
+
+  sealed trait Generator {
+    def withInits(inits: Tree*): InitAddingGenerator = InitAddingGenerator(this, inits)
+  }
+  sealed trait InnerGenerator extends Generator {
+    def outer: Generator
+    def transformOuter(f: Generator ⇒ Generator): InnerGenerator
+  }
+  case class MappingGenerator(outer: Generator, f: Closure) extends InnerGenerator {
+    def transformOuter(f: Generator ⇒ Generator): MappingGenerator = copy(outer = f(outer))
+  }
+  case class FlatMappingGenerator(outer: Generator, valName: TermName, innerGenerator: Generator) extends InnerGenerator {
+    def transformOuter(f: Generator ⇒ Generator): FlatMappingGenerator = copy(outer = f(outer))
+  }
+  case class FilteringGenerator(outer: Generator, f: Closure) extends InnerGenerator {
+    def transformOuter(f: Generator ⇒ Generator): FilteringGenerator = copy(outer = f(outer))
+  }
+  case class RangeGenerator(start: Tree, end: Tree, by: Tree, inclusive: Tree) extends Generator
+  case class ArrayGenerator(array: Tree) extends Generator
+  case class InitAddingGenerator(outer: Generator, inits: Seq[Tree]) extends InnerGenerator {
+    def transformOuter(f: Generator ⇒ Generator): InitAddingGenerator = copy(outer = f(outer))
+  }
+
+  sealed trait TerminalOperation
+  case class Foreach(f: Closure) extends TerminalOperation
+  case class FoldLeft(init: Tree, f: Closure2) extends TerminalOperation
+  case class Reduce(tpe: Type, f: Closure2) extends TerminalOperation
+
+  case class Sum(numeric: Tree) extends TerminalOperation
+  case class Min(ordering: Tree) extends TerminalOperation
+  case class Max(ordering: Tree) extends TerminalOperation
+  case object Size extends TerminalOperation
+
+  case class OperationChain(generator: Generator, terminal: TerminalOperation)
+}
